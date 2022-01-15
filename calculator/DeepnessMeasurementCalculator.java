3c6f4a78b8ca3fd48082023de9a9b6264e37ed36
nataemi <nataliagalda7@gmail.com>
2022-01-15 12:45:05 +0100

    day1 completed

---
+++ calculator/DeepnessMeasurementCalculator.java
@@ -0,0 +1,42 @@
+package pl.ngalda.adventofcode2021.calculator;
+
+import pl.ngalda.adventofcode2021.util.FileReader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeepnessMeasurementCalculator {
+
+    public static int calculateDepths(){
+        List<Integer> deepness = FileReader.readLinesAsInt("day1.txt");
+        return getDeepnessIncreasedCounter(deepness);
+    }
+
+    private static int getDeepnessIncreasedCounter(List<Integer> deepness) {
+        int deepnessIncreasedCounter = 0;
+        for(int i = 0; i< deepness.size(); i++){
+            if(i>0){
+                if (deepness.get(i-1) < deepness.get(i)){
+                    deepnessIncreasedCounter++;
+                }
+            }
+        }
+        return deepnessIncreasedCounter;
+    }
+
+    public static int calculateDepthsSumsOfThree(){
+        List<Integer> deepness = FileReader.readLinesAsInt("day1.txt");
+        List<Integer> sumsOfThree = new ArrayList<>();
+        for(int i=0 ; i< deepness.size(); i++){
+            if(i>1){
+                sumsOfThree.add(deepness.get(i) + deepness.get(i-1) + deepness.get(i-2));
+            }
+        }
+        return getDeepnessIncreasedCounter(sumsOfThree);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(calculateDepths());
+        System.out.println(calculateDepthsSumsOfThree());
+    }
+}
---
+++ util/FileReader.java
@@ -0,0 +1,46 @@
+package pl.ngalda.adventofcode2021.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FileReader {
+    private static InputStream readFile(final String fileName){
+        InputStream ioStream = FileReader.class
+                .getClassLoader()
+                .getResourceAsStream(fileName);
+
+        if (ioStream == null) {
+            throw new IllegalArgumentException(fileName + " is not found");
+        }
+        return ioStream;
+    }
+
+    public static List<String> readLines(final String fileName)
+    {
+        List<String> lines = new ArrayList<>();
+        try (InputStreamReader isr = new InputStreamReader(readFile(fileName));
+             BufferedReader br = new BufferedReader(isr))
+        {
+            String line;
+            while ((line = br.readLine()) != null) {
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return lines;
+    }
+
+    public static List<Integer> readLinesAsInt(final String fileName)
+    {
+        return readLines(fileName)
+                .stream()
+                .map(Integer::valueOf)
+                .collect(Collectors.toList());
+    }
+}
