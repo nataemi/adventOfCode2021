8246f5ae0beb897b3220bed1b03b16c5a0db56f1
nataemi <nataliagalda7@gmail.com>
2022-02-04 20:05:57 +0100

    day6 part 2 solution

--- solutions/day6/LanternFishSimulator.java
+++ solutions/day6/LanternFishSimulator.java
@@ -2,48 +2,35 @@ package pl.ngalda.adventofcode2021.solutions.day6;
 
 import pl.ngalda.adventofcode2021.util.FileReader;
 
-import java.util.HashMap;
+import java.math.BigInteger;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
 
 public class LanternFishSimulator {
     public static void main(String[] args) {
         int dayCount = 256;
         List<Integer> lanternFishDays = FileReader.readComaSeperatedValuesAsInt("day6.txt");
-        Map<Integer, Integer> fishLivingInDays = new HashMap<>();
-        fishLivingInDays.put(0,0);
-        fishLivingInDays.put(1,0);
-        fishLivingInDays.put(2,0);
-        fishLivingInDays.put(3,0);
-        fishLivingInDays.put(4,0);
-        fishLivingInDays.put(5,0);
-        fishLivingInDays.put(6,0);
-        fishLivingInDays.put(7,0);
-        fishLivingInDays.put(8,0);
+        BigInteger[] fishLivingInDays = new BigInteger[9];
+        for (int j = 0; j <= 8; j++) {
+            fishLivingInDays[j] = BigInteger.ZERO;
+        }
 
-        for(int daysLeft : lanternFishDays){
-            fishLivingInDays.put(daysLeft, fishLivingInDays.get(daysLeft) + 1);
+        for (int daysLeft : lanternFishDays) {
+            fishLivingInDays[daysLeft] = fishLivingInDays[daysLeft].add(BigInteger.ONE);
         }
 
         for (int i = 0; i < dayCount; i++) {
-            Map<Integer,Integer> fishLivingInDaysCopy =
-                    fishLivingInDays.entrySet().stream()
-                            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-            int zeros = fishLivingInDays.get(0);
-            for(int j = 0; j<= 7; j++){
-                fishLivingInDays.put(j, fishLivingInDaysCopy.get(j+1));
+            BigInteger zeros = fishLivingInDays[0];
+            for (int j = 0; j <= 7; ++j) {
+                fishLivingInDays[j] = fishLivingInDays[j + 1];
             }
-            fishLivingInDays.put(8,0);
-            for(int k = 0; k< zeros; k++) {
-                fishLivingInDays.put(6, fishLivingInDays.get(6) + 1);
-                fishLivingInDays.put(8, fishLivingInDays.get(8) + 1);
-            }
+            fishLivingInDays[6] = fishLivingInDays[6].add(zeros);
+            fishLivingInDays[8] = zeros;
         }
-        int totalSum = 0;
-        for(Integer i : fishLivingInDays.keySet()){
-            totalSum += fishLivingInDays.get(i);
+        BigInteger totalSum = BigInteger.ZERO;
+        for (int i = 0; i < fishLivingInDays.length; i++) {
+            totalSum = totalSum.add(fishLivingInDays[i]);
         }
-        System.out.println(totalSum);
+        System.out.println(Arrays.stream(fishLivingInDays).reduce(BigInteger.ZERO, BigInteger::add));
     }
 }
